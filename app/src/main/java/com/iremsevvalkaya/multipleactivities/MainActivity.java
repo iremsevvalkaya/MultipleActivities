cd9f1b3e60b1df601c24cb894a6b1837e3df3566
iremsevvalkaya <iremsevvalceng@gmail.com>
2024-03-20 19:09:06 +0300

    Initial commit

---
+++ app/src/main/java/com/iremsevvalkaya/multipleactivities/MainActivity.java
@@ -0,0 +1,33 @@
+package com.iremsevvalkaya.multipleactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    String userName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        userName= "";
+    }
+    public void changeActivity(View view){
+        userName = editText.getText().toString();
+        // Kullanıcının girdiği veriyi alıp userName'e eşitledik.
+        //Intent  sayfalar aras
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class );
+        intent.putExtra("userInput", userName);
+        startActivity(intent);
+
+
+
+    }
+}
